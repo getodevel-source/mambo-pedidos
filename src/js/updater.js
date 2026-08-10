9dd35c6987a0a4d07b58a1a77ddf929cca5239c7
Mambo Pedidos <juans@mambo-pedidos.local>
2026-08-10 07:19:39 -0300

    chore(release): bump version to 2.0.4 (fix inline onclick CSP)

--- src/js/updater.js
+++ src/js/updater.js
@@ -12,7 +12,7 @@
  */
 
 const AppUpdater = {
-  CURRENT_VERSION: '2.0.3',
+  CURRENT_VERSION: '2.0.4',
   REPO_URL: 'https://github.com/getodevel-source/mambo-pedidos',
   latestVersion: null,
   latestNotes: null,
@@ -35,7 +35,7 @@ const AppUpdater = {
   },
 
   getCurrentVersion() {
-    return this.CURRENT_VERSION || '2.0.3';
+    return this.CURRENT_VERSION || '2.0.4';
   },
 
   /**
