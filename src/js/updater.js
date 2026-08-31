12d010fb3fde876ca9ddd90ea8030c75c0f6cbb1
Geto <getodevel@gmail.com>
2026-08-31 19:02:32 -0300

    chore(release): bump version to v2.2.8 (all QA-gate fixes in the release code: smoke degrade, distros, e2e thresholds)

--- src/js/updater.js
+++ src/js/updater.js
@@ -12,7 +12,7 @@
  */
 
 const AppUpdater = {
-  CURRENT_VERSION: '2.2.7',
+  CURRENT_VERSION: '2.2.8',
   REPO_URL: 'https://github.com/getodevel-source/mambo-pedidos',
   latestVersion: null,
   latestNotes: null,
@@ -35,7 +35,7 @@ const AppUpdater = {
   },
 
   getCurrentVersion() {
-    return this.CURRENT_VERSION || '2.2.7';
+    return this.CURRENT_VERSION || '2.2.8';
   },
 
   /**
