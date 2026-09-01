4b2f09a0d789f45350be843eebd75b148f7ebd55
Geto <getodevel@gmail.com>
2026-08-31 21:50:46 -0300

    chore(release): bump version to v2.2.12 (macOS window controls + platform-neutral WC test)

--- src/js/tests.js
+++ src/js/tests.js
@@ -2640,7 +2640,7 @@ const Tests = {
 		}
 
 		this.assert(
-			AppUpdater.CURRENT_VERSION === '2.2.11',
+			AppUpdater.CURRENT_VERSION === '2.2.12',
 			"AppUpdater CURRENT_VERSION configurado en 2.1.0",
 		);
 		this.assert(
